715c4815b8066e075bcddca1e8ce6667fe7a36d9
Nikolay <dnolnque@gmail.com>
2023-05-23 14:43:33 +0300

    minor fixes

--- .tina/__generated__/client.js
+++ .tina/__generated__/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '035e1913af3b6d7ea5ad390354583e2408dbf0c6', queries });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries });
 export default client;
   
