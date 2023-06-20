d66e6c7e3efc7efcfaec8a6358f3d83f2e0555e7
Nikolay <dnolnque@gmail.com>
2023-06-20 17:28:31 +0300

    new homepage design

--- src/components/Homepage/constants.js
+++ src/components/Homepage/constants.js
@@ -1,115 +1,89 @@
-const ExploreApisList = [ 
-    {
-      title: "Smart Wallets API",
-      description: 'Maximize the security and user experience of your apps by leveraging smart contract wallets.',
-      href: '/docs/smart-wallet-api/smart-wallets'
-    },
-    {
-      title: "Relay API",
-      description: 'Use our relay service to cover user transaction fees and remove friction.',
-      href:'/docs/smart-wallet-api/relay'
-    },
-    {
-      title: "Trade API",
-      description: 'Enable DeFi functionalities including token swaps, real-time price information, and more in your apps.',
-      href:'/docs/category/trade-api'
-    },
-    {
-      title: "Notifications API",
-      description: 'Maximize the security and user experience of your apps by leveraging smart contract wallets.',
-      href:'/docs/category/notification-api'
-    },
-    {
-      title: "Admin API",
-      description: 'Create and manage custodial wallets and ERC20 tokens for payments, loyalty and more.',
-      href:'/docs/category/admin-api'
-    },
-    {
-      title: "Data APIs (Coming soon)",
-      description: 'Access real-time indexed data for balances, tokens, and transactions with ease.'
-    }
-  ];
+const ExploreApisList = [
+  {
+    title: 'Smart Wallets API',
+    description:
+      'Maximize the security and user experience of your apps by leveraging smart contract wallets.',
+    href: '/docs/smart-wallet-api/smart-wallets',
+  },
+  {
+    title: 'Relay API',
+    description:
+      'Use our relay service to cover user transaction fees and remove friction.',
+    href: '/docs/smart-wallet-api/relay',
+  },
+  {
+    title: 'Trade API',
+    description:
+      'Enable DeFi functionalities including token swaps, real-time price information, and more in your apps.',
+    href: '/docs/category/trade-api',
+  },
+  {
+    title: 'Notifications API',
+    description:
+      'Maximize the security and user experience of your apps by leveraging smart contract wallets.',
+    href: '/docs/category/notification-api',
+  },
+  {
+    title: 'Admin API',
+    description:
+      'Create and manage custodial wallets and ERC20 tokens for payments, loyalty and more.',
+    href: '/docs/category/admin-api',
+  },
+  {
+    title: 'Data APIs (Coming soon)',
+    description:
+      'Access real-time indexed data for balances, tokens, and transactions with ease.',
+  },
+]
 
-  const MainFeatureList = [
-    {
-      image: "img/img1.svg",
-      imageHover: "img/img7.svg",
-      title: "Learn the basics",
-      description:
-        "Learn all the basics about the Fuse network, including network details, wallets, bridges and more.",
-      link: "/docs/category/intro-to-fuse",
-    },
-    {
-      image: "img/img2.svg",
-      imageHover: "img/img8.svg",
-      title: "Fuse SDK",
-      description:
-        "Develop seamless UX with Fuse SDK on Fuse network, leveraging account abstraction, gasless transactions, and more.",
-      link: "/docs/developers/fuse-sdk/",
-    },
-    {
-      image: "img/img3.svg",
-      imageHover: "img/img9.svg",
-      title: "Fuse V2",
-      description:
-        "Discover everything you need to know about Fuse v2 and how we're revolutionizing various industries with web3 technology.",
-      link: "/docs/category/fuse-v2-next-chapter/",
-    },
-    {
-      image: "img/img4.svg",
-      imageHover: "img/img10.svg",
-      title: "Deploy a contract",
-      description:
-        "Discover how to confidently deploy secure and reliable smart contracts using a range of powerful tools designed to simplify the process.",
-      link: "/docs/developers/deploying-smart-contracts/",
-    },
-    {
-      image: "img/img5.svg",
-      imageHover: "img/img11.svg",
-      title: "Validate the network",
-      description:
-        "Participate in consensus of the Fuse network by operating a node and becoming a Fuse validator.",
-      link: "/docs/validators/participating-in-network-consensus/",
-    },
-    {
-      image: "img/img6.svg",
-      imageHover: "img/img12.svg",
-      title: "Participate in governance",
-      description:
-        "Learn about governance and how to shape the future of Fuse network through the FIP framework.",
-      link: "/docs/basics/intro-to-fuse/fuse-governance-and-development/",
-    },
-  ];
+const MainFeatureList = [
+  {
+    image: 'img/new-ms-p.svg',
+    title: 'Fuse Docs',
+    description:
+      'Learn all the basics about the Fuse network, including network details, wallets, bridges and more.',
+    link: '/docs/category/intro-to-fuse',
+  },
+  {
+    image: 'img/new-ms-p1.svg',
 
-  const SocialsList =[
-    {
-        icon: "img/git.png",
-        title: "Github",
-        description:
-          "All repos maintained by the core contributors of flow.",
-        href: "https://github.com/fuseio",
-      },{
-        icon: "img/forum.png",
-        title: "Forum",
-        description:
-          "A place to discuss proposals, check updates, and find answers.",
-        href: "https://forum.fuse.io/",
-      },{
-        icon: "img/discord.png",
-        title: "Discord",
-        description:
-          "Talk directly to the developer community.",
-        href: "https://discord.com/invite/jpPMeSZ",
-      },{
-        icon: "img/twitter.png",
-        title: "Twitter",
-        description:
-          "Checkout the latest happenings on flow.",
-        href: "https://twitter.com/fuse_network",
-      },
-  ]
+    title: 'Fuse Basics',
+    description:
+      'Develop seamless UX with Fuse SDK on Fuse network, leveraging account abstraction, gasless transactions, and more.',
+    link: '/docs/developers/fuse-sdk/',
+  },
+  {
+    image: 'img/new-ms-p2.svg',
 
-  export { ExploreApisList, MainFeatureList, SocialsList };
-  
-  
-  
+    title: 'Fuse Guides',
+    description:
+      "Discover everything you need to know about Fuse v2 and how we're revolutionizing various industries with web3 technology.",
+    link: '/docs/category/fuse-v2-next-chapter/',
+  },
+  {
+    image: 'img/new-ms-p3.svg',
+
+    title: 'Fuse 2.0',
+    description:
+      'Discover how to confidently deploy secure and reliable smart contracts using a range of powerful tools designed to simplify the process.',
+    link: '/docs/developers/deploying-smart-contracts/',
+  },
+]
+
+const GovernanceList = [
+  {
+    icon: 'img/git.png',
+    title: 'Fip`s',
+    description: `Governance is currently split among the following \n actors.....`,
+    href: 'https://github.com/fuseio',
+  },
+  {
+    icon: 'img/forum.png',
+    title: 'Fuse Assembly',
+    description:
+      'Fuse Assembly is the first step in transitioning full governance and ownership over the Fuse platform to the community.',
+    href: 'https://forum.fuse.io/',
+  },
+]
+
+export { ExploreApisList, MainFeatureList, GovernanceList }
