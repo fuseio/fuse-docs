00899f94da214c8ab15ffff4cea2b9afcf901b90
Lior Agnin <lior.agnin@gmail.com>
2023-09-18 21:03:47 +0300

    fixes

--- docs/data-layer-api/sidebar.js
+++ docs/data-layer-api/sidebar.js
@@ -1,14 +1,1 @@
-module.exports = [
-  {
-    type: 'doc',
-    id: 'data-layer-api/auth',
-    label: 'Auth',
-    className: 'api-method post',
-  },
-  {
-    type: 'doc',
-    id: 'data-layer-api/get-wallet-actions-4337',
-    label: 'Get Wallet Actions (4337)',
-    className: 'api-method get',
-  },
-]
+module.exports = [{"type":"doc","id":"data-layer-api/smart-wallets-api-v-2-data-layer"},{"type":"category","label":"Data layer","items":[{"type":"doc","id":"data-layer-api/auth","label":"Auth","className":"api-method post"},{"type":"doc","id":"data-layer-api/get-wallet-actions-4337","label":"Get Wallet Actions (4337)","className":"api-method get"}]}];
