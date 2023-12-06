092398de73c6748d12996ce4793c835800ec882f
emmaodia <odia.emma@gmail.com>
2023-12-06 09:28:53 +0100

    update: Smart Wallet API v2

---
+++ docs/smart-wallet-api-v/sidebar.js
@@ -0,0 +1,61 @@
+module.exports = [
+  { type: "doc", id: "smart-wallet-api-v/smart-wallets-api-v2" },
+  {
+    type: "category",
+    label: "Smart Wallets v2",
+    items: [
+      {
+        type: "doc",
+        id: "smart-wallet-api-v/auth",
+        label: "Auth",
+        className: "api-method post",
+      },
+      {
+        type: "doc",
+        id: "smart-wallet-api-v/get-wallet-actions",
+        label: "Get Wallet Actions",
+        className: "api-method get",
+      },
+    ],
+  },
+];
+
+// module.exports = [
+//   { type: "doc", id: "smart-wallet-api-v/smart-wallets-api-v2" },
+//   {
+//     type: "category",
+//     label: "Smart Wallets v2",
+//     items: [
+//       {
+//         type: "doc",
+//         id: "smart-wallet-api/auth",
+//         label: "Auth",
+//         className: "api-method post",
+//       },
+//       {
+//         type: "doc",
+//         id: "smart-wallet-api/create-wallet",
+//         label: "Create Wallet",
+//         className: "api-method post",
+//       },
+//       {
+//         type: "doc",
+//         id: "smart-wallet-api/get-wallet",
+//         label: "Get Wallet",
+//         className: "api-method get",
+//       },
+//       {
+//         type: "doc",
+//         id: "smart-wallet-api/relay",
+//         label: "Relay",
+//         className: "api-method post",
+//       },
+//       {
+//         type: "doc",
+//         id: "smart-wallet-api/get-historical-transactions",
+//         label: "Get Historical Transactions",
+//         className: "api-method get",
+//       },
+//     ],
+//   },
+// ];
