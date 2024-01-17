3f3611fcecff537e012b994f0ce720c374b6ae1d
Lior Agnin <lior.agnin@gmail.com>
2024-01-17 09:38:28 +0200

    Squashed and merged staging into master, to prevent conflicts

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
 import { Widget } from '@happyreact/react';
- 
+
 import styles from './styles.module.css';
 
 import '@happyreact/react/theme.css';
- 
+
 const VotedYes = () => {
   return <span>Thanks for your feedback. We are glad you like it :)</span>;
 };
- 
+
 const VotedNo = () => {
   return <span>Thanks for your feedback. We will try to improve :(</span>;
 };
- 
+
 export default function Feedback({ resource, token }) {
   const [reaction, setReaction] = useState(null);
- 
+
   const isReacted = reaction === 'Yes' || reaction === 'No';
   const _resource = String(resource).replace(/\//g, '-');
- 
+
   const handleReaction = (params) => {
     setReaction(params.icon);
   };
- 
+
   return (
     <div id="feedback">
       <h3>Was this page helpful?</h3>
