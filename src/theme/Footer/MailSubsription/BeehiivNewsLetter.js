d9fa15edbaef4613b4eb6b4bf13b0354bef68163
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-07-01 11:30:27 +0000

    Add Beehiiv newsletter form

---
+++ src/theme/Footer/MailSubsription/BeehiivNewsLetter.js
@@ -0,0 +1,7 @@
+const BeehiivNewsLetter = () => {
+  return (
+    <iframe src="https://embeds.beehiiv.com/17e5f33a-1d02-48fa-acc5-156f339521cd?slim=true" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style="margin: 0; border-radius: 0px !important; background-color: transparent;"></iframe>
+  )
+}
+
+export default BeehiivNewsLetter
