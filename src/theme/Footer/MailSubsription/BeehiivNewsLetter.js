6f2421b451b7d9a43f33742b90ff4c602b0d6f14
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-07-01 11:50:39 +0000

    Fix React is not defined

--- src/theme/Footer/MailSubsription/BeehiivNewsLetter.js
+++ src/theme/Footer/MailSubsription/BeehiivNewsLetter.js
@@ -1,6 +1,12 @@
+import React from 'react'
+import styles from './styles.module.css'
+
 const BeehiivNewsLetter = () => {
   return (
-    <iframe src="https://embeds.beehiiv.com/17e5f33a-1d02-48fa-acc5-156f339521cd?slim=true" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style="margin: 0; border-radius: 0px !important; background-color: transparent;"></iframe>
+    <div className={styles.mailChimp}>
+      <div className={styles.mailChimp__title}>Subscribe to Newsletter</div>
+      <iframe src="https://embeds.beehiiv.com/b585ef59-5562-4611-928d-6ff081772079?slim=true" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style={{margin: "0", borderRadius: "0px !important", backgroundColor: "transparent"}}></iframe>
+    </div>
   )
 }
 
