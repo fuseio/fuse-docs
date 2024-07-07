3aa503c8221362ac1498e45b6df12ffb6270cb08
Lior Agnin <lior.agnin@gmail.com>
2024-07-07 11:26:31 +0300

    minor fix

--- .tina/config.jsx
+++ .tina/config.jsx
@@ -9,7 +9,7 @@ import title from "title";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
-  "docusaurusV3";
+  "master";
 
 const DocsCollection = {
   name: "doc",
