38929c005885b0f2d1284e90d518a6b2e31b7668
Lior Agnin <lior.agnin@gmail.com>
2025-06-12 08:14:42 +0300

    Update .gitignore to exclude TinaCMS generated files and add new generated files for TinaCMS including GraphQL schemas, queries, and types.

---
+++ .tina/__generated__/client.d.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/Users/liorag/Documents/development/fuse-docs/.tina/__generated__/.cache/1749705131529', url: 'https://content.tinajs.io/1.5/content/f6010f30-8b7b-4f45-bdfe-c1e8774bbb4d/github/staging', token: 'd215e8c58c9b6aa353d6cb8bbec3b6029d634312', queries,  });
+export default client;
+  
